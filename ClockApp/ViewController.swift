342594c39af0888153acababa3a76070ccbb9e0a
David Svensson <davidsvson@gmail.com>
2021-11-18 15:27:18 +0100

    chessClock

--- ClockApp/ViewController.swift
+++ ClockApp/ViewController.swift
@@ -14,49 +14,88 @@ class ViewController: UIViewController {
     
     var timer: Timer?
     
+    let startTime = 6.0
+    
+    var player1time = 0.0
+    var player2time = 0.0
+    var player1active = true
+    
+    var lastTime = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loadg the view.
         
-        formatter.timeStyle = .medium
+        resetPlayerTime()
         
-        updateTimeLabel()
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: updateTimeLabel(timer:))
-        
-//        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) {
-//            timer in
-//            let date = Date()
-//            let timeString = self.formatter.string(from: date)
-//
-//            self.timeLabel.text = timeString
-//
-//        }
+        updateTimelabel()
     }
     
-
-    func updateTimeLabel(timer: Timer? = nil) {
-        let date = Date()
-        let timeString = formatter.string(from: date)
+    func resetPlayerTime() {
+        player1time = startTime
+        player2time = startTime
+    }
+    
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        if timer != nil  {
+            switchPlayer()
+        } else {
+            startClock()
+        }
         
-        timeLabel.text = timeString
+    }
+    
+    func switchPlayer() {
+        player1active.toggle()
+    }
+    
+    func startClock() {
+        resetPlayerTime()
         
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateTime(timer:))
+        lastTime = Date()
+    }
+    
+    func updateTime(timer: Timer? = nil) {
+        
+        let newTime = Date()
+        let deltaTime = newTime.timeIntervalSince(lastTime)
+        
+        if player1active {
+            player1time -= deltaTime
+        } else {
+            player2time -= deltaTime
+        }
+        
+        lastTime = newTime
+        
+        updateTimelabel()
+        
+        if player2time < 0.1 || player1time < 0.1 {
+            timer?.invalidate()
+        }
+    }
+    
+    func updateTimelabel() {
+        let newTimeString = "\(String(format: "%.2f", player1time))   :   \(String(format: "%.2f", player2time))"
+        
+        timeLabel.text = newTimeString
     }
     
     deinit {
-//        if let timer = timer {
-//            timer.invalidate()
-//        }
-       
         timer?.invalidate()
-        
     }
-
-
 }
 
 
-
+//class Player {
+//
+//
+//    var timeLeft : Double
+//
+//    init(timeLeft: Double) {
+//        self.timeLeft = timeLeft
+//    }
+//}
 
 
 
